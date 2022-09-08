96021277691a764b29d79430be62d371cd310fb2
thomasbalmant <thomas_dru@hotmail.com>
2022-09-08 16:39:18 -0300

    add button and header

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,16 +11,47 @@ class HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Text('Contador: $counter'),
-        onTap: () {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, color: Colors.deepOrange, size: 40.0),
+        onPressed: () {
           setState(() {
             counter++;
           });
         },
+      ),
+      body: Center(
+          child: GestureDetector(
+        child: Text(
+          'Contador: $counter',
+          // ignore: prefer_const_constructors
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
       )),
     );
   }
 }
+
+
+// class HomePageState extends State<HomePage> {
+//   int counter = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//           child: GestureDetector(
+//         child: Text('Contador: $counter'),
+//         onTap: () {
+//           setState(() {
+//             counter++;
+//           });
+//         },
+//       )),
+//     );
+//   }
+// }
