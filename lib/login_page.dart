bd49779f1e791037a7b058a9ab57e513ff65eae7
thomasbalmant <thomas_dru@hotmail.com>
2022-09-18 11:31:40 -0300

    login inputs

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,55 @@
 import 'package:flutter/material.dart';
 
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   State<LoginPage> createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SingleChildScrollView(
+//       child: SizedBox(
+//         width: MediaQuery.of(context).size.width,
+//         height: MediaQuery.of(context).size.height,
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               // ignore: prefer_const_constructors
+//               TextField(
+//                 keyboardType: TextInputType.emailAddress,
+//                 // ignore: prefer_const_constructors
+//                 decoration: InputDecoration(
+//                     labelText: 'Email', border: const OutlineInputBorder()),
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               const TextField(
+//                 obscureText: true,
+//                 decoration: InputDecoration(
+//                   labelText: 'Password',
+//                   border: OutlineInputBorder(),
+//                 ),
+//               ),
+//               FloatingActionButton(
+//                 onPressed: () {},
+//                 tooltip: 'Login',
+//                 child: const Text('Entrar'),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     ));
+//   }
+// }
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -11,41 +61,46 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // ignore: prefer_const_constructors
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                // ignore: prefer_const_constructors
-                decoration: InputDecoration(
-                    labelText: 'Email', border: const OutlineInputBorder()),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Email', border: OutlineInputBorder())),
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              FloatingActionButton(
-                onPressed: () {},
-                tooltip: 'Login',
-                child: const Text('Entrar'),
-              )
-            ],
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Password', border: OutlineInputBorder()),
+                )
+              ],
+            ),
           ),
         ),
       ),
-    ));
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(height: 50.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Login',
+        child: const Text('Login'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
