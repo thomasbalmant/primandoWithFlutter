ec4ec149922ddc489852b72840de0b5978bc4e7a
thomasbalmant <thomas_dru@hotmail.com>
2022-09-19 11:32:36 -0300

    another bg image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,60 +10,76 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
+
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                // child: Image.asset('assets/images/logo.jpg'),
+              ),
+              Container(
+                height: 20,
+              ),
+              TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: const OutlineInputBorder())),
+              const SizedBox(
+                height: 20,
+              ),
+              // ignore: prefer_const_constructors
+              TextField(
+                onChanged: (text) {
+                  password = text;
+                },
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Password', border: OutlineInputBorder()),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
+        backgroundColor: Colors.black,
       ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset('assets/images/logo.jpg'),
-                ),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    // ignore: prefer_const_constructors
-                    decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: const OutlineInputBorder())),
-                const SizedBox(
-                  height: 20,
-                ),
-                // ignore: prefer_const_constructors
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      labelText: 'Password', border: OutlineInputBorder()),
-                )
-              ],
-            ),
-          ),
-        ),
+      body: Stack(
+        children: [
+          SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/images/moon.jfif',
+                fit: BoxFit.cover,
+              )),
+          _body()
+        ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0),
-      ),
+      // bottomNavigationBar: BottomAppBar(
+      //   shape: const CircularNotchedRectangle(),
+      //   child: Container(height: 50.0),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (email.isNotEmpty && password.isNotEmpty) {
@@ -73,8 +89,9 @@ class _LoginPageState extends State<LoginPage> {
         },
         tooltip: 'Login',
         child: const Text('Login'),
+        backgroundColor: Colors.pink,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
