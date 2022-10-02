28110eb0895c8cc6b5a76168ca8f1ee26e9ed81f
thomasbalmant <thomas_dru@hotmail.com>
2022-10-02 10:51:31 -0300

    Drawer

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -29,6 +29,7 @@ class _LoginPageState extends State<LoginPage> {
       onVerticalDragDown: (value) =>
           FocusScope.of(context).requestFocus(FocusNode()),
       child: Scaffold(
+        drawer: Drawer(),
         appBar: AppBar(
           title: const Text(
             'Login',
